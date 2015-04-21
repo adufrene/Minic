f9b4af3ac1c1af42d032be8e2dd82482f550d1cb
zarend <zacharyarend@gmail.com>
2015-04-20 18:30:52 -0700

    refactor Iloc data defn to be less OO

--- src/Mini/Iloc.hs
+++ src/Mini/Iloc.hs
@@ -21,23 +21,23 @@ retReg = 1
 ccReg :: Reg
 ccReg = -1
 
-data Iloc = Arithmetic | Boolean | Comparison | Branching | Loads | Stores | Invocation | Allocation | IO | Moves 
-
-data Arithmetic = Add Reg Reg Reg
+data Iloc = 
+                  Add Reg Reg Reg
                 | Addi Reg Immed Reg
                 | Div Reg Reg Reg
                 | Mult Reg Reg Reg
+                | Multi Reg Immed Reg
                 | Sub Reg Reg Reg
                 | Rsubi Reg Immed Reg
 
-data Boolean = And Reg Reg Reg
+             | And Reg Reg Reg
              | Or Reg Reg Reg
              | Xori Reg Immed Reg
 
-data Comparison = Comp Reg Reg
+                | Comp Reg Reg
                 | Compi Reg Immed
 
-data Branching = Cbreq Label Label
+               | Cbreq Label Label
                | Cbrge Label Label
                | Cbrgt Label Label
                | Cbrle Label Label
@@ -46,7 +46,7 @@ data Branching = Cbreq Label Label
                | Jumpi Label
                | Brz Reg Label Label
 
-data Loads = Loadai Reg Immed Reg
+           | Loadai Reg Immed Reg
            | Loadglobal Id Reg
            | Loadinargument Id Immed Reg
            | Loadret Reg
@@ -54,23 +54,23 @@ data Loads = Loadai Reg Immed Reg
            | Restoreformal Id Immed
            | Computeglobaladdress Id Reg
 
-data Stores = Storeai Reg Reg Immed
+            | Storeai Reg Reg Immed
             | Storeglobal Reg Id
             | Storeinargument Reg Id Immed
             | Storeoutargument Reg Immed
             | Storeret Reg
 
-data Invocation = Call Label
+                | Call Label
                 | RetILOC
 
-data Allocation = New Immed Reg
+                | New Immed Reg
                 | Del Reg
 
-data IoILOC = PrintILOC Reg
+             | PrintILOC Reg
              | Println Reg
              | ReadILOC Reg
 
-data Moves = Mov Reg Reg
+           | Mov Reg Reg
            | Movi Immed Reg
            | Moveq Immed Reg
            | Movge Immed Reg
@@ -88,24 +88,22 @@ showReg regNum = "r" ++ show regNum
 showIlocHelper :: String -> [String] -> String
 showIlocHelper name args = name ++ " " ++ intercalate ", " args
 
-instance Show Arithmetic where
+instance Show Iloc where
    show (Add r1 r2 r3) = showIlocHelper "add" $ map showReg [r1, r2, r3]
    show (Addi r1 i1 r2) = showIlocHelper "addi" [showReg r1, show i1, showReg r2]
    show (Div r1 r2 r3) = showIlocHelper "div" $ map showReg [r1, r2, r3]
    show (Mult r1 r2 r3) = showIlocHelper "mult" $ map showReg [r1, r2, r3]
+   show (Multi r1 i1 r2) = showIlocHelper "multi" [showReg r1, show i1, showReg r2]
    show (Sub r1 r2 r3) = showIlocHelper "sub" $ map showReg [r1, r2, r3]
    show (Rsubi r1 i1 r2) = showIlocHelper "rsubi" [showReg r1, show i1, showReg r2]
 
-instance Show Boolean where
    show (And r1 r2 r3) = showIlocHelper "and" $ map showReg [r1, r2, r3]
    show (Or r1 r2 r3) = showIlocHelper "or" $ map showReg [r1, r2, r3]
    show (Xori r1 i1 r2) = showIlocHelper "xori" [showReg r1, show i1, showReg r2]
 
-instance Show Comparison where
    show (Comp r1 r2) = showIlocHelper "comp" [showReg r1, showReg r2, condCodeReg]
    show (Compi r1 i1) = showIlocHelper "compi" [showReg r1, show i1, condCodeReg]
 
-instance Show Branching where
    show (Cbreq l1 l2) = showIlocHelper "cbreq" [condCodeReg, l1, l2]
    show (Cbrge l1 l2) = showIlocHelper "cbrge" [condCodeReg, l1, l2]
    show (Cbrgt l1 l2) = showIlocHelper "cbrgt" [condCodeReg, l1, l2]
@@ -115,7 +113,6 @@ instance Show Branching where
    show (Jumpi l1) = showIlocHelper "jumpi" [l1]
    show (Brz r1 l1 l2) = showIlocHelper "brz" [showReg r1, l1, l2]
 
-instance Show Loads where
    show (Loadai r1 i1 r2) = showIlocHelper "loadai" [showReg r1, show i1, showReg r2]
    show (Loadglobal s1 r1) = showIlocHelper "loadglobal" [s1, showReg r1]
    show (Loadinargument s1 i1 r1) = showIlocHelper "loadinargument" [s1, show i1, showReg r1]
@@ -124,27 +121,22 @@ instance Show Loads where
    show (Restoreformal s1 i1) = showIlocHelper "restoreformal" [s1, show i1]
    show (Computeglobaladdress s1 r1) = showIlocHelper "computeglobaladdress" [s1, showReg r1]
 
-instance Show Stores where
    show (Storeai r1 r2 i1) = showIlocHelper "storeai" [showReg r1, showReg r2, show i1]
    show (Storeglobal r1 s1) = showIlocHelper "storeglobal" [showReg r1, s1]
    show (Storeinargument r1 s1 i1) = showIlocHelper "storeinargument" [showReg r1, s1, showReg i1]
    show (Storeoutargument r1 i1) = showIlocHelper "storeoutargument" [showReg r1, show i1]
    show (Storeret r1) = showIlocHelper "storeret" [showReg r1]
 
-instance Show Invocation where
    show (Call l1) = showIlocHelper "call" [l1]
    show RetILOC = "ret"
 
-instance Show Allocation where
    show (New i1 r1) = showIlocHelper "new" [show i1, showReg r1]
    show (Del r1) = showIlocHelper "del" [showReg r1]
 
-instance Show IoILOC where
    show (PrintILOC r1) = showIlocHelper "print" [showReg r1]
    show (Println r1) = showIlocHelper "println" [showReg r1]
    show (ReadILOC r1) = showIlocHelper "read" [showReg r1]
 
-instance Show Moves where
    show (Mov r1 r2) = showIlocHelper "mov" [showReg r1, showReg r2]
    show (Moveq i1 r1) = showIlocHelper "moveq" [show i1, showReg r1]
    show (Movge i1 r1) = showIlocHelper "movge" [show i1, showReg r1]
