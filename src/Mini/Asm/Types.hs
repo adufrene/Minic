2dd8ca6b1f909166399412e084a3d67fbca73c57
Andy DuFrene <andy.dufrene94@gmail.com>
2015-04-30 22:10:05 -0700

    call & ret, still need scan

--- src/Mini/Asm/Types.hs
+++ src/Mini/Asm/Types.hs
@@ -1,8 +1,28 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Mini.Asm.Types where
+
+import Data.Char
+import Data.Data
 
 import Mini.Iloc.Types
 
 type AsmReg = Reg
+
+data AsmReg = Rax
+            | Rbx
+            | Rcx
+            | Rdx
+            | Rsp
+            | Rbp
+            | Rsi
+            | Rdi
+            | RegNum Int
+            deriving (Eq, Data, Typeable)
+
+instance Show RealReg where
+        show (RegNum i) = "%r" ++ show i
+        show reg = "%" ++ map toLower (toConstr reg)
 
 data Asm = AsmAdd
          | AsmSub
@@ -22,14 +42,10 @@ data Asm = AsmAdd
          | AsmStoreInArg
          | AsmStoreOutArg
          | AsmStoreRet
-         | AsmCall
+         | AsmCall Label
          | AsmRet
-         | AsmNew
-         | AsmDel
-         | AsmPrint
-         | AsmPrintln
-         | AsmRead
          | AsmMov AsmReg AsmReg
+         | AsmMovm Label AsmReg
          | AsmMovi Immed AsmReg
          | AsmCMoveq Immed AsmReg
          | AsmcMovgeq Immed AsmReg
@@ -39,15 +55,45 @@ data Asm = AsmAdd
          | AsmCmovneq Immed AsmReg
          deriving (Eq)
 
-regStr :: Reg -> String
-regStr r = " %r" ++ show r
+wordSize :: Int
+wordSize = 8
+
+printLabel :: Label
+printLabel = ".PRINT"
+
+printlnLabel :: Label
+printlnLabel = ".PRINTLN"
+
+printf :: Label
+printf = "printf"
+
+scanLabel :: Label
+scanLabel = ".SCAN"
+
+scanf :: Label
+scanf = "scanf"
+
+free :: Label
+free = "free"
+
+malloc :: Label
+malloc = "malloc"
+
+regStr :: AsmReg -> String
+regStr r = " " ++ show r
+
+labStr :: Label -> String
+labStr l = " $" ++ l
 
 immStr :: Immed -> String
 immStr i = " $" ++ show i
 
 instance Show Iloc where
-        show (AsmMov r1 r2) = "mov" ++ regStr r1 ++ regStr r2
-        show (AsmMovi i r) = "mov" ++ immStr i ++ regStr r
+        show (AsmCall l) = "call " ++ l
+        show AsmRet = "ret"
+        show (AsmMov r1 r2) = "movq" ++ regStr r1 ++ regStr r2
+        show (AsmMovm l r) = "movq" ++ labStr l ++ regStr r
+        show (AsmMovi i r) = "movq" ++ immStr i ++ regStr r
         show (AsmCmoveq i r) = "cmoveq" ++ immStr i ++ regStr r
         show (AsmCmovgeq i r) = "cmovgeq" ++ immStr i ++ regStr r
         show (AsmCmovgq i r) = "cmovgq" ++ immStr i ++ regStr r
@@ -57,12 +103,39 @@ instance Show Iloc where
         show asm = undefined
 
 ilocToAsm :: Iloc -> [Asm]
-ilocToAsm (Mov r1 r2) = [AsmMov r1 r2]
-ilocToAsm (Movi i r) = [AsmMovi i r]
-ilocToAsm (Moveq i r) = [AsmCmoveq i r]
-ilocToAsm (Movge i r) = [AsmCmovgeq i r]
-ilocToAsm (Movgt i r) = [AsmCmovgq i r]
-ilocToAsm (Movle i r) = [AsmCmovleq i r]
-ilocToAsm (Movlt i r) = [AsmCmovlq i r]
-ilocToAsm (Movne i r) = [AsmCmovneq i r]
+ilocToAsm (Call l) = [AsmCall l]
+ilocToAsm RetILOC = [AsmRet]
+ilocToAsm (New i r) = createNew i r
+ilocToAsm (Del r) = createDelete r
+ilocToAsm (PrintILOC r) = createPrint r False
+ilocToAsm (Println r) = createPrint r True
+ilocToAsm (ReadILOC r) = createRead r
+ilocToAsm (Mov r1 r2) = [AsmMov (RegNum r1) (RegNum r2)]
+ilocToAsm (Movi i r) = [AsmMovi i $ RegNum r]
+ilocToAsm (Moveq i r) = [AsmCmoveq i $ RegNum r]
+ilocToAsm (Movge i r) = [AsmCmovgeq i $ RegNum r]
+ilocToAsm (Movgt i r) = [AsmCmovgq i $ RegNum r]
+ilocToAsm (Movle i r) = [AsmCmovleq i $ RegNum r]
+ilocToAsm (Movlt i r) = [AsmCmovlq i $ RegNum r]
+ilocToAsm (Movne i r) = [AsmCmovneq i $ RegNum r]
 ilocToAsm iloc = undefined
+
+createNew :: Immed -> Reg
+createNew words res = [ AsmMovi (words * wordSize) Rdi
+                      , AsmCall malloc
+                      , AsmMov Rax (RegNum res) ]
+
+createDelete :: Reg -> [Asm]
+createDelete r = [ AsmMov (RegNum r) Rdi
+                 , AsmCall free ]
+
+createPrint :: Reg -> Bool -> [Asm]
+createPrint r endl = [ AsmMovm printString Rdi
+                     , AsmMov (RegNum r) Rsi
+                     , AsmMovi 0 Rax
+                     , AsmCall printf ]
+    where printString = if endl then printlnLabel else printLabel
+
+{- store memory location in rsi, then copy from memory to reg -}
+createRead :: Reg -> [Asm]
+createRead r = undefined
