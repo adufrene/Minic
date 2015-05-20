1a52c8c3edb5ea23ae83a93b5605ca94c4e920c3
Andy DuFrene <andy.dufrene94@gmail.com>
2015-05-20 11:25:22 -0700

    stupid seg faults

--- scripts/testBenchmarks.sh
+++ scripts/testBenchmarks.sh
@@ -6,10 +6,51 @@ set -u
 MINI_EXE=mCompile
 BENCHMARK_DIR="$(realpath $(dirname ${BASH_SOURCE})/../benchmarks)"
 
+REMOTE_LOGIN=''
+
 cleanup() {
     cd ..
     rm -rf $TMP_DIR
 }
+
+while getopts r: opt
+do
+    case $opt in
+        r)
+            REMOTE_LOGIN=$OPTARG
+            ;;
+        \?)
+#            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+testSSH() {
+    if [[ -z $REMOTE_LOGIN ]]
+    then
+        return 0
+    else
+        ssh $REMOTE_LOGIN 'true'
+    fi
+}
+
+if [[ -n $REMOTE_LOGIN ]]
+then
+    echo "Testing ssh..."
+    ssh $REMOTE_LOGIN 'true'
+    if [[ $? -eq 0 ]]
+    then
+        echo "Success!"
+    else
+        echo "Remote login failed"
+        exit 1
+    fi
+fi
 
 TMP_DIR=$(mktemp -d -p .)
 cd $TMP_DIR
@@ -22,10 +63,24 @@ runTest() {
     miniFile=$1
     input=$2
     output=$3
+    tempFile="$filename".tmp
 
     $MINI_EXE $miniFile
-    gcc "$filename".s -o $filename
-    $filename < $input | diff - $output &> /dev/null
+
+    if [[ -n $REMOTE_LOGIN ]]
+    then
+        remote_input=$(basename "$input")
+
+        scp -q "$filename.s" "$REMOTE_LOGIN:"
+        scp -q "$input" "$REMOTE_LOGIN:"
+        ssh $REMOTE_LOGIN "gcc $filename.s -o $filename && ./$filename < $remote_input | cat &> $tempFile"
+        scp -q "$REMOTE_LOGIN:$tempFile" .
+        ssh $REMOTE_LOGIN "rm $filename $filename.s $remote_input $tempFile"
+    else
+        gcc "$filename.s" -o $filename && $filename < $input | cat &> $tempFile
+    fi
+
+    diff $tempFile $output &> /dev/null
 }
 
 EXIT_STATUS=0
